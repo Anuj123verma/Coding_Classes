39d11984f264b9637ec40da1a8ba5492f855ad12
Anuj123Verma <anuj17026@iiitd.ac.in>
2022-03-07 19:47:25 +0530

    coins.java updated

---
+++ sorting_searching/coins.java
@@ -0,0 +1,13 @@
+package sorting_searching;
+
+import java.util.Arrays;
+
+public class coins {
+    public int maxCoins(int[] A) {
+        Arrays.sort(A);
+        int res = 0, n = A.length;
+        for (int i = n / 3; i < n; i += 2)
+            res += A[i];
+        return res;
+    }
+}
