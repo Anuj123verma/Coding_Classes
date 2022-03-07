e8b48ee32d6f0efe696036a68f317bdbfa02a751
Anuj123Verma <anuj17026@iiitd.ac.in>
2022-03-07 20:01:34 +0530

    updated

--- sorting_searching/biasedStandingg.java
+++ sorting_searching/biasedStandingg.java
@@ -6,20 +6,23 @@ public class biasedStandingg {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        long n, badness;
-        long sum = 0;
+        int n, badness;
+        long preferred_position;
         String team_name;
         while (t-- > 0) {
-            sum = 0;
-            n = sc.nextLong();
-            long ans = (n * (n + 1)) / 2;
-            while (n-- > 0) {
+            n = sc.nextInt();
+            long[] arr = new long[n];
+            for (int i = 0; i < n; i++) {
                 team_name = sc.next();
-                sum += sc.nextLong();
+                preferred_position = sc.nextInt();
+                arr[i] = preferred_position;
             }
-            badness = Math.abs(sum - ans);
+            Arrays.sort(arr);
+            badness = 0;
+            for (int i = 1; i <= n; i++) {
+                badness += Math.abs(i - arr[i - 1]);
+            }
             System.out.println(badness);
         }
-
     }
 }
