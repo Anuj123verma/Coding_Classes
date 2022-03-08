d71a7f96f679403cc8c159716188d0943960ca33
Anuj123Verma <anuj17026@iiitd.ac.in>
2022-03-08 19:21:50 +0530

    binarySearch added

---
+++ sorting_searching/search.java
@@ -0,0 +1,42 @@
+package sorting_searching;
+
+import java.util.Scanner;
+
+public class search {
+    public static int binarySearch(int[] arr, int s, int e, int target) {
+        int low = s;
+        int high = e;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int target = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(binarySearch(arr, 0, n - 1, target));
+    }
+}
+
+/*
+ * 2
+ * 1 2 3 4 5
+ * 2
+ * 1
+ * l h l h
+ */
