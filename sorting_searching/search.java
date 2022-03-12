65ed8be6701779796f45f6c4252560f5ad95e434
anuj.verma <anuj.verma@idfcfirstbank.com>
2022-03-12 18:31:33 +0530

    updated

--- sorting_searching/search.java
+++ sorting_searching/search.java
@@ -22,14 +22,15 @@ public class search {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int target = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int target = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            System.out.println(binarySearch(arr, 0, n - 1, target));
         }
-        System.out.println(binarySearch(arr, 0, n - 1, target));
     }
 }
 
